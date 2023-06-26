0f1414ee78c54bb91fedab4be54a468d9eae37a8
Leon C <yck124@gmail.com>
2023-06-26 21:55:55 +1200

    add improvement to text extraction results display

--- app.py
+++ app.py
@@ -41,11 +41,9 @@ def extract_text():
     results = reader.readtext(cv2_image) # results will be a list containing multiple tuples corresponding to the number of words that were extracted. Each tuple has 3 elements - first element is a list of text box coordinates [x.y], 2nd element is the extracted text, and 3rd element is the model confidence level
 
     # Check if text was extracted
-    if len(results) > 0:
-        # Retrieve all extracted text from results and place into single string
-        text = ""
-        for word in results:
-            text += word[1] + " "
+    if results:
+        # Retrieve all extracted text from results and join into a single string, seperated by new line
+        text = '\n'.join(text[1] for text in results)
         # Set response as extracted text
         response = {'extracted_text': text}
     else:
